475f2ec9f0f2023a66e9e4d571d66e0ca0e5aee4
Daniel Yoo <daniehjyoo@gmail.com>
2023-05-04 18:59:17 -0500

    Update function to redirect to character_page

--- main.py
+++ main.py
@@ -21,12 +21,9 @@ def get_character():
         server = request.form['server']
         response = requests.get(f"https://xivapi.com/character/search?name={name}&server={server}&private_key={api_key}")
         data = response.json()
-        # check if the request was successful
-        #if response.status_code == 200:
-            # extract the character id from the response data
-        character_id = data['Results'][0]['ID']
-        print(character_id)
-        return redirect(url_for('character_profile', character_id=character_id))
+        if response.status_code == 200 and data['Pagination']['Results'] > 0:
+            character_id = data['Results'][0]['ID']
+            return redirect(url_for('character_profile', character_id=character_id))
     return render_template('index.html')
 
     #     # use the character id to get the character data
@@ -44,18 +41,16 @@ def get_character():
     #      # return an error message
     #      return jsonify({'error': 'Failed to retrieve character data'})
     # return 'Hello, ' + name + '! You play on the ' + server + ' server.'
-@app.route('/character')
-@app.route('/character/<int:character_id>')
+
+@app.route('/character/<int:character_id>', methods=['GET','POST'])
 def character_profile(character_id):
     url = f'https://xivapi.com/character/{character_id}?private_key={api_key}'
     response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        character_name = data['Character']['Name']
-        server = data['Character']['Server']
-        return render_template('character.html', character_name=character_name, server=server, character_id=character_id)
-    else:
-        return f'Error: {response.status_code}'
+    data = response.json()
+    character_name = data['Character']['Name']
+    server = data['Character']['Server']
+    return render_template('character.html', character_name=character_name, server=server, character_id=character_id)
+
     
 
 if __name__ == '__main__':
